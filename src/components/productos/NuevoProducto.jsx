0d679df2892962726f0e8aa5f1bb59875f16523b
radiceadriann@gmail.com <radiceadriann@gmail.com>
2020-03-19 11:45:36 -0300

    addForms

--- src/components/productos/NuevoProducto.jsx
+++ src/components/productos/NuevoProducto.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState} from "react";
 
 const NuevoProducto = () => {
-    return (
-        <h1>Nuevo Producto</h1>
-    );
-}
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+  const onChange = e=> {
+      setProducto({
+          ...producto,
+          [e.target.name] : e.target.value
+      })
+  }
+  const [producto, setProducto] = useState({
+      nombre: '',
+      precio: 0
+  })
+  const {nombre, precio} = producto;
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-a font-weight-bold">
+              Agregar nuevo prouducto
+            </h2>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <label>Nombre Producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                  value={nombre}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label>Precio</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio Producto"
+                  name="precio"
+                  value={precio}
+                  onChange={onChange}
+                />
+              </div>
+              <button 
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >Agrear</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NuevoProducto;
--- src/components/productos/Productos.jsx
+++ src/components/productos/Productos.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 
 const Producto = () => {
     return (
-        <h1>Producto</h1>
+        <Fragment>
+            <h2 className="text-center my-5">Listado de productos</h2>
+            <table className="table table-striped">
+                <thead classNmae="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </Fragment>
     );
 }
 
