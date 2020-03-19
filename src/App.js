51a8cf671d61196a9b8accd00b1045c178999a34
radiceadriann@gmail.com <radiceadriann@gmail.com>
2020-03-19 10:45:32 -0300

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import Header from './components/Header'
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Producto from "./components/productos/Productos";
+import NuevoProducto from "./components/productos/NuevoProducto";
+import EditarProducto from "./components/productos/EditarProducto";
 function App() {
   return (
-      <Header/>
+    <Router>
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Producto}/>
+          <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+          <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
       <div className="container">
-        <h1>CRUD</h1>
-        <a href="/products/nuevo">Agregar Producto</a>
+        <h1>
+          <Link to={"/"} className="text-light">
+            CRUD Redux
+          </Link>
+        </h1>
+        <Link
+          to={"/productos/nuevo"}
+          className="btn btn-danger nuevo-post d-block d-md-inline-block"
+        >
+          Agregar Producto &#43;
+        </Link>
       </div>
     </nav>
   );
